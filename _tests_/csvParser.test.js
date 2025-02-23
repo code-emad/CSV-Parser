46418d0acd6107f6dfcb3b25e61eba3250e15685
Emad Islam <emad.islam@pimberly.com>
2025-02-23 14:54:04 +0000

    added csv file and included test to read this

--- _tests_/csvParser.test.js
+++ _tests_/csvParser.test.js
@@ -1,21 +1,15 @@
-const {returnNumber, readInputCSV} = require('../csvParser')
-const inputCSV1 = require('../CSV_files/1.csv')
+const {readInputCSV} = require('../csvParser')
+const path = require('path')
 
-describe('Sample test block', () => {
-    it('should return a number', () => {
-        expect(typeof returnNumber()).toBe('number')
-    })
-
-    it('should return 123', () => {
-        expect(returnNumber()).toBe(123)
-    })
-})
+const inputCSV1 = path.join(__dirname, '../CSV_files/1.csv')
 
 describe('readInputCSV', () => {
     it('if input is undefined, then a string should be returned to say there is no input', () => {
         expect(typeof readInputCSV(undefined)).toBe(typeof 'string')
     })
-    it('if input is defined, then an array should be returned', () => {
-        expect(Array.isArray(readInputCSV(inputCSV1))).toBe(true)
-    })
+    it('if input is defined, then an array should be returned', async () => {
+        const data = await readInputCSV(inputCSV1); // ✅ Await the Promise
+        expect(Array.isArray(data)).toBe(true);
+    });
+
 });
--- csvParser.js
+++ csvParser.js
@@ -1,19 +1,23 @@
-const Papa = require('papaparse')
-const fs = require('fs')
-
-function returnNumber() {
-    return 123
-}
+const Papa = require("papaparse");
+const fs = require("fs");
+const csv = require("csv-parser");
 
 //read input csv and return arrat of objects
 function readInputCSV(inputCSV) {
-    if (inputCSV === undefined) {return "No input file specified"}
+  if (inputCSV === undefined) {
+    return "No input file specified";
+  }
 
-    
-     return []
+  return new Promise((resolve, reject) => {
+    const inputCSVArray = [];
+    fs.createReadStream(inputCSV)
+      .pipe(csv())
+      .on("data", (data) => inputCSVArray.push(data))
+      .on("end", () => {
+        console.log(inputCSVArray);
+        resolve(inputCSVArray);
+      });
+  });
 }
 
-
-
-
-module.exports = {returnNumber, readInputCSV}
+module.exports = { readInputCSV };
