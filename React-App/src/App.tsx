87801a1d6c821f0104f0f3816a1c13d01925ec91
Emad Islam <emad.islam@pimberly.com>
2025-03-16 17:27:37 +0000

    moving files around

--- React-App/src/App.tsx
+++ React-App/src/App.tsx
@@ -1,15 +1,19 @@
 import React, { useRef } from "react";
 import './App.css';
+import {readInputCSV, processCSVtoJSON} from './utils/csvParser'
 
 
 const App: React.FC = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
       console.log("Selected file:", file.name);
       // You can add logic here to process the file
+
+      const data = await readInputCSV(file)
+
     }
   };
 
